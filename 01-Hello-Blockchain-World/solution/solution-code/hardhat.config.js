caada5534fa4df7b27f93c5f887f9dc299b0cd82
Lukas Radovansky <radovluk@fel.cvut.cz>
2025-02-06 15:19:08 +0100

    🔧 Update config files

--- 01-Hello-Blockchain-World/solution/solution-code/hardhat.config.js
+++ 01-Hello-Blockchain-World/solution/solution-code/hardhat.config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.0",
+  solidity: "0.8.28",
   networks: {
     sepolia: {
       url: `https://sepolia.infura.io/v3/${process.env.INFURA_API_KEY}`,
