b7c8ea4b3c1cebc5317f963495248dfd598f87b6
Lukas Radovansky <radovluk@fel.cvut.cz>
2025-02-18 11:42:40 +0100

    📦 Migration from dotenv to Hardhat configuration variables

--- 01-Hello-Blockchain-World/solution/solution-code/hardhat.config.js
+++ 01-Hello-Blockchain-World/solution/solution-code/hardhat.config.js
@@ -1,5 +1,7 @@
 require("@nomicfoundation/hardhat-toolbox");
-require("dotenv").config();
+
+const INFURA_API_KEY = vars.get("INFURA_API_KEY");
+const SEPOLIA_PRIVATE_KEY = vars.get("SEPOLIA_PRIVATE_KEY");
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
